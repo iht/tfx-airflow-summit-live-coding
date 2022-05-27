c0dce7bc9ee4740808a1481fd7041caeb3eb18fe
Israel Herraiz <ihr@google.com>
2022-05-27 19:09:27 +0200

    WIP

--- 01_tfx_pipeline.py
+++ 01_tfx_pipeline.py
@@ -1,9 +1,12 @@
 import os
 
+from datetime import datetime
+
 import tfx.v1 as tfx
 from tfx.components import CsvExampleGen
 from tfx.orchestration import metadata
 from tfx.extensions.google_cloud_big_query.example_gen.component import BigQueryExampleGen
+from tfx.orchestration.airflow.airflow_dag_runner import AirflowDagRunner
 
 
 def create_pipeline(data_location: str,
@@ -25,8 +28,25 @@ def create_pipeline(data_location: str,
     return pipeline
 
 
-def run_pipeline():
-    pass
+def run_pipeline(data_location: str,
+                 pipeline_name: str,
+                 pipeline_root: str):
+    p: tfx.dsl.Pipeline = create_pipeline(data_location=data_location,
+                                          pipeline_name=pipeline_name,
+                                          pipeline_root=pipeline_root)
+    airflow_config = {
+        'schedule_interval': None,
+        'start_date': datetime(2022, 5, 1)}
+
+    runner = AirflowDagRunner(config=airflow_config)
+
+    return runner.run(p)
 
 
-DAG = "something"
+PIPELINE_NAME = "my_first_tfx_pipeline"
+PIPELINE_ROOT = "/tmp/airflow-summit-live/tfx/"
+DATA_LOCATION = "/Users/ihr/projects/tfx-airflow-summit-live-coding/data/iris_dataset.csv"
+
+DAG = run_pipeline(data_location=DATA_LOCATION,
+                   pipeline_root=PIPELINE_ROOT,
+                   pipeline_name=PIPELINE_NAME)
